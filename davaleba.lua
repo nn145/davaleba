e15036675d7969146e41b9b25ef8a051f4fff492
nn145 <118995431+nn145@users.noreply.github.com>
2023-07-03 22:22:37 +0400

    Add files via upload

---
+++ davaleba.lua
@@ -0,0 +1,28 @@
+-- N1
+sum = 0
+
+for i = 1, 100 do
+  sum = sum + i
+end
+
+print("The sum of numbers from 1 to 100 is: " .. sum)
+
+-- N2
+number = 10
+
+randomNumber = math.random(1, 20)
+number = number + randomNumber
+
+print("After adding a random number, the variable is now: " .. number)
+
+
+-- N3
+randomNumber = math.random(1, 20)
+
+if randomNumber > 10 then
+  print(true)
+else
+  print("false")
+end
+
+-- N4 da N5 ver gavige, Table-is axsnaze ar viyavi
